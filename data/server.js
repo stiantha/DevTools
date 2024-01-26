9378feb1a0034266c4f7b0455f18bc3e8185793c
Stian Tharaldsen <stiantharaldsen@hotmail.com>
2024-01-26 13:47:16 +0100

    first commit

---
+++ data/server.js
@@ -0,0 +1,18 @@
+// Setup
+// npm init -y
+// npm install express
+// npm install dotenv
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Launch
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
