c628d3c5a0be53285187d5674e96164bfeb5dcd3
Stian Tharaldsen <stiantharaldsen@hotmail.com>
2024-01-30 13:08:15 +0100

    chore: update 1 file and delete 3 files

--- server/server.js
+++ server/server.js
@@ -5,15 +5,21 @@
 // Setup
 require('dotenv').config();
 const express = require('express');
-const app = express();
+const toolsRoutes = require('./routes/tools');
 
-// Routes
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// App
+const app = express();
+// Middleware
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
 });
 
-// Launch
-const PORT = process.env.PORT || 3000;
+// Routes
+app.use('/api/tools', toolsRoutes);
+
+// Listener
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
